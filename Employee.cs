28218a78defd9a2f9f7fa3ef3b1034aa7141222c
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:36:02 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo1
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("Employee Default Constructor Called");
+        }
+
+        public Employee(int id)
+        {
+            Console.WriteLine("Employee Parameterized Constructor with 1 Parameter Called");
+        }
+
+        public Employee(int id, string name)
+        {
+            Console.WriteLine("Employee Parameterized Constructor with 2 Parameters Called");
+        }
+
+        static Employee()
+        {
+            Console.WriteLine("Employee Static Constructor called");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            Employee emp1 = new Employee(101);
+
+            Employee emp2 = new Employee(101, "Robert");
+
+            Console.ReadKey();
+        }
+    }
+}
